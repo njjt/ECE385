8ab5480659fac13ad507859083c49472900c4c7a
njjt <71482363+njjt@users.noreply.github.com>
2021-09-26 20:41:22 -0500

    Add files via upload

--- lookahead_adder.sv
+++ lookahead_adder.sv
@@ -9,9 +9,14 @@ module lookahead_adder (
      * Insert code here to implement a CLA adder.
      * Your code should be completly combinational (don't use always_ff or always_latch).
      * Feel free to create sub-modules or other files. */
-   logic c0,c1,c2
-   clu_4 clu1(.A(A[3:0]),.B(B[3:0]),.cin(cin),.S(S[3:0]),.cout(c0));
-   clu_4 clu2(.A(A[7:4]),.B(B[7:4]),.cin(c0),.S(S[7:4]),.cout(c1));
-   clu_4 clu3(.A(A[11:8]),.B(B[11:8]),.cin(c1),.S(S[11:8]),.cout(c2));
-   clu_4 clu4(.A(A[15:12]),.B(B[15:12]),.cin(c2),.S(S[15:12]),.cout(cout));
+
+	logic [3:0] p,g,cout_1;
+	clu_4 clu0(.P(p[3:0]),.G(g[3:0]),.cin(0),.cout(cout)); 
+   cla4 cla1(.A(A[3:0]),.B(B[3:0]),.cin(cin),.S(S[3:0]),.pout(p[0]),.gout(g[0]));
+
+   cla4 cla2(.A(A[7:4]),.B(B[7:4]),.cin(cout_1[0]),.S(S[7:4]),.pout(p[1]),.gout(g[1]));
+   cla4 cla3(.A(A[11:8]),.B(B[11:8]),.cin(cout_1[1]),.S(S[11:8]),.pout(p[2]),.gout(g[2]));
+   cla4 cla4(.A(A[15:12]),.B(B[15:12]),.cin(cout_1[2]),.S(S[15:12]),.pout(p[3]),.gout(g[3]));
+	
+	//clu_4 clu0(.P(),.G(),.cin(cout_1[]),.S(S[15:12]),.cout(cout)) 
 endmodule
